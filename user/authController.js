4af17437bfc9b37254618c192b2481eaed9797b7
khto-dev <kevinhau.toronto@gmail.com>
2023-03-17 18:16:25 -0400

    feat(jwt): adds JWT to keep users logged in

--- user/authController.js
+++ user/authController.js
@@ -1,4 +1,6 @@
-const User = require('./user');
+const User = require("./user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 
 // Handle Errors
@@ -35,6 +37,11 @@ const handleErrors = (err) => {
     return errors;
 }
 
+// JWT Token
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET_KEY);
+};
+
 // Exports for router
 // GET requests
 module.exports.signup_get = (req, res) => {
@@ -43,12 +50,18 @@ module.exports.signup_get = (req, res) => {
 module.exports.login_get = (req, res) => {
     res.render("login");
 };
+module.exports.logout_get = (req, res) => {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.redirect("/");
+}
 // POST requests
 module.exports.signup_post = async (req, res) => {
     const { email, password, displayname } = req.body;
 
     try {
         const user = await User.create({ email, password, displayname });
+        const token = createToken(user._id);
+        res.cookie("jwt", token, { httpOnly: true });
         res.status(201).json({ user: user._id });
     } catch(err) {
         const errors = handleErrors(err);
@@ -60,6 +73,9 @@ module.exports.login_post = async (req, res) => {
 
     try {
         const user = await User.login(email, password);
+        const token = createToken(user._id);
+        console.log(user._id);
+        res.cookie("jwt", token, { httpOnly: true });
         res.status(200).json({ user: user._id });
     } catch(err) {
         const errors = handleErrors(err);
--- user/authRoutes.js
+++ user/authRoutes.js
@@ -4,9 +4,13 @@ const authController = require("./authController");
 
 const router = Router();
 
+// Sign up routes
 router.get("/signup", authController.signup_get);
 router.post("/signup", authController.signup_post);
+// Log in routes
 router.get("/login", authController.login_get);
 router.post("/login", authController.login_post);
+// Log out routes
+router.get("/logout", authController.logout_get);
 
 module.exports = router;
